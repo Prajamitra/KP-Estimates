2847587563e05ddecb58da02fdf8e8a6ddeb2b25
Prajamitra <118843409+Prajamitra@users.noreply.github.com>
2026-02-24 11:37:25 +0530

    Update MSM_Mz_Mb_Bin.R

--- MSM_Mz_Mb_Bin.R
+++ MSM_Mz_Mb_Bin.R
@@ -12,12 +12,10 @@ library(HDInterval)
 
 #############################################
 
-tot=500000
-
-th=20
-lw=tot*0.5
+tot=500000  # No of posteriror samples
+th=20       # Thining   
+lw=tot*0.5  # Burnin
 up=tot
-
 
 ########## Initial parameter specification ###############
 
@@ -915,6 +913,7 @@ clab = 2
 caxis = 2
 
 plot(w, Prob3_3(w), type="h", col="blue",lwd=2, xlim=c(0,0.05), ylim=c(0,1), xlab=expression(~ lambda), ylab=expression(P(beta[3]>lambda ~ "|" ~  data)) ,cex.lab=clab,cex.axis=caxis)
+
 
 
 
